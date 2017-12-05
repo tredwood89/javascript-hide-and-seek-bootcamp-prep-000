90785bd3855b33d5a457cde2a32e9504d2274e0c
tredwood89 <tredwood89@gmail.com>
2017-12-05 01:21:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,3 +17,12 @@ function increaseRankBy(n){
     ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
   }
 }
+
+function deepestChild() {
+
+  var nodes = document.querySelectorAll('#grand-node div');
+  var deepest = nodes[nodes.length- 1];
+
+    if(!nodes.length) return false;
+    return nodes[nodes.length-1];
+}
