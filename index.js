5bc0bdcb583da508b4f64899562a57eae0ce7b5b
tredwood89 <tredwood89@gmail.com>
2017-12-05 01:00:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ function getFirstSelector(selector) {
 
     return document.querySelector(selector)
 }
+
+function nestedTarget() {
+
+    return document.getElementById('nested').querySelectorAll('div.target')
+}
